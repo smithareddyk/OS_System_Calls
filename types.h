b6e2d752e40c1035554d3fbf6c66f265fde8d0d5
smithareddyk <96351189+smithareddyk@users.noreply.github.com>
2025-02-09 15:04:47 -0500

    Delete types.h

--- types.h
+++
@@ -1,9 +0,0 @@
-typedef unsigned int   uint;
-typedef unsigned short ushort;
-typedef unsigned char  uchar;
-typedef uint pde_t;
-struct processstat{
-	uint creationTime;
-	uint endTime;
-	uint totalTime;
-};
