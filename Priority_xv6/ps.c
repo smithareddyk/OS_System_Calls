763d6d4fe2d732871f2993774262f7d27f48611d
smithareddyk <96351189+smithareddyk@users.noreply.github.com>
2025-02-09 15:00:02 -0500

    Edited

---
+++ Priority_xv6/ps.c
@@ -0,0 +1,10 @@
+#include "types.h"
+#include "user.h"
+#include"stat.h"
+#define ticks 1000
+
+int main(int argc, char *argv[])
+{
+        ps();
+        exit();
+}
---
+++ Priority_xv6/types.h
@@ -0,0 +1,9 @@
+typedef unsigned int   uint;
+typedef unsigned short ushort;
+typedef unsigned char  uchar;
+typedef uint pde_t;
+struct processstat{
+	uint creationTime;
+	uint endTime;
+	uint totalTime;
+};
